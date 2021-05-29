3d4d096b06dd8f8afbc50b14dda57d57e0e8ff0b
Tanmoy Sarker <tanmoy@Tanmoys-MacBook-Air.local>
2021-05-29 23:42:57 +0600

    initial commit

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,12 +11,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-// @react-native-firebase/app
-import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
-// @react-native-firebase/messaging
-import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
-// react-native-firebase
-import io.invertase.firebase.RNFirebasePackage;
+// react-native-push-notification
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 public class PackageList {
   private Application application;
@@ -62,9 +58,7 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
-      new ReactNativeFirebaseAppPackage(),
-      new ReactNativeFirebaseMessagingPackage(),
-      new RNFirebasePackage()
+      new ReactNativePushNotificationPackage()
     ));
   }
 }
